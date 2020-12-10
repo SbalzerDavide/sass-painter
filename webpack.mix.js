a6328704fb072cf2efe26dc2b31b7a27369309d8
Davide <Sbalzerdavide@hotmail.it>
2020-12-10 18:42:25 +0100

    background hero

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ let mix = require('laravel-mix');
 mix.js('src/js/main.js', 'dist/js')
     .sass('src/scss/main.scss', 'dist/css')
     .options({
-        processCssUrls: false
+        processCssUrls: false,
     });
 
 // Full API
