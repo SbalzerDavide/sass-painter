60f53aa209426e8e99939cac6dc003794a9a84e8
Davide <Sbalzerdavide@hotmail.it>
2020-12-12 15:58:50 +0100

    map

---
+++ src/js/main.js
@@ -0,0 +1,121 @@
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        map: [
+            {
+                section: 'Procucts',
+                element:[
+                    {
+                        name: 'Corel Painter',
+                        path: '#'
+                    },
+                    {
+                        name: 'PartcleShop',
+                        path: '#'
+                    },
+                    {
+                        name: 'Painter Essential',
+                        path: '#'
+                    },
+                    {
+                        name: 'Special Offers',
+                        path: '#'
+                    },
+                    {
+                        name: 'Free Trials',
+                        path: '#'
+                    },
+                    {
+                        name: 'All products',
+                        path: '#'
+                    },
+                ]
+            },
+            {
+                section: 'Quick Links',
+                element:[
+                    {
+                        name: 'Licensing',
+                        path: '#'
+                    },
+                    {
+                        name: 'Tutorials & Tips',
+                        path: '#'
+                    },
+                    {
+                        name: 'Webinars',
+                        path: '#'
+                    },
+                    {
+                        name: 'The Painter Factory',
+                        path: '#'
+                    },
+                    {
+                        name: 'Discovery Center',
+                        path: '#'
+                    },
+                    {
+                        name: 'Beta Programs',
+                        path: '#'
+                    },
+                ]
+            },
+            {
+                section: 'Support',
+                element:[
+                    {
+                        name: 'Register yourr product',
+                        path: '#'
+                    },
+                    {
+                        name: 'Patches & Updates',
+                        path: '#'
+                    },
+                    {
+                        name: 'Knowledgebase',
+                        path: '#'
+                    },
+                    {
+                        name: 'Unistall instructions',
+                        path: '#'
+                    },
+                    {
+                        name: 'Contact us',
+                        path: '#'
+                    },
+                ]
+            },
+            {
+                section: 'Procucts',
+                element:[
+                    {
+                        name: 'company information',
+                        path: '#'
+                    },
+                    {
+                        name: 'Newsroom',
+                        path: '#'
+                    },
+                    {
+                        name: 'Careers',
+                        path: '#'
+                    },
+                    {
+                        name: 'Partner Program',
+                        path: '#'
+                    },
+                    {
+                        name: 'Legal information',
+                        path: '#'
+                    },
+                    {
+                        name: 'EULA',
+                        path: '#'
+                    },
+                ]
+            },
+        ],
+    }
+})
